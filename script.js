dd254f63472ab5ef52147d3eb7fd5a500143b866
Sacrastic <124285630+Sacrastic@users.noreply.github.com>
2024-02-04 10:42:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function redirectToKnow() {
+    window.location.href = '404.html';
+}
+function redirectToReg() {
+  window.location.href = 'reg.html';
+}
+function redirectToReg2() {
+  window.location.href = 'reg2.html';
+}
