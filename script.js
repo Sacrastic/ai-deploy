32c4680824cb029ed72885cb3fcb35b4123b3e61
Sacrastic <124285630+Sacrastic@users.noreply.github.com>
2024-02-04 10:41:39 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function redirectToKnow() {
-    window.location.href = '404.html';
-}
-function redirectToReg() {
-  window.location.href = 'reg.html';
-}
-function redirectToReg2() {
-  window.location.href = 'reg2.html';
-}
